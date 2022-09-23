8b2d0fc284e62eff2043371c52d65f9494bad14b
Levi <megaraidsserver@gmail.com>
2022-09-23 11:29:47 +0000

    Update 4 files

--- components/Header.tsx
+++ components/Header.tsx
@@ -6,13 +6,13 @@ const Header: NextPage<{ title: string }> = ({ title }) => {
     <Head>
       <title>{title}</title>
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <meta property="og:title" content="Crypto Store Exchange" />
+      <meta property="og:title" content="HcRaid | Crypto Store Exchange" />
       <meta property="og:type" content="website" />
       <meta
         property="og:description"
         content="Exchange crypto for store credit"
       />
-      <meta name="theme-color" content="#2e5792"></meta>
+      <meta name="theme-color" content="#696969"></meta>
     </Head>
   );
 };
